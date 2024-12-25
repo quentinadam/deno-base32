27e9d4a8ed905d86561b576bee7bb9a3913d5373
Quentin Adam <quentinadam@gmail.com>
2024-12-25 23:09:57 +0100

    Minor documentation changes. Bump version to v0.1.1.

--- base32.ts
+++ base32.ts
@@ -32,23 +32,50 @@ function convert({ input, inputBase, outputBase, convertRemainingBits }: {
 }
 
 function getAlphabet(alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567') {
-  if (alphabet.length === 33) {
-    return alphabet;
-  }
+  assert(alphabet.length === 32 || alphabet.length === 33, 'Alphabet must be 32 or 33 characters long');
   if (alphabet.length === 32) {
-    return alphabet + '=';
+    alphabet += '=';
   }
-  throw new Error(`Invalid alphabet ${alphabet}`);
+  assert(new Set(alphabet).size === 33, 'Alphabet must not contain duplicate characters');
+  return alphabet;
+}
+
+/** Options for {@linkcode encode}. */
+export interface EncodeOptions {
+  /**
+   * The alphabet to use.
+   * The alphabet must be a string of 32 unique characters (or 33 unique characters to specify the padding character to use).
+   *
+   * @default {'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567='}
+   */
+  alphabet?: string;
+  /**
+   * Whether the encoded string should be padded.
+   *
+   * @default {true}
+   */
+  padding?: boolean;
+}
+
+/** Options for {@linkcode decode}. */
+export interface DecodeOptions {
+  /**
+   * The alphabet to use.
+   * The alphabet must be a string of 32 unique characters (or 33 unique characters to specify the padding character to use).
+   *
+   * @default {'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567='}
+   */
+  alphabet?: string;
 }
 
 /**
  * Encodes a Uint8Array buffer into a base32 string.
  *
  * @param buffer The buffer to encode.
- * @param options Optionally specify the alphabet to use and if the encoded string should be padded. The alphabet must be a 32-character string, or 33 characters if specifying the padding character to use.
+ * @param options The options to use for encoding.
  * @returns The base32 encoded string.
  */
-export function encode(buffer: Uint8Array, options?: { alphabet?: string; padding?: boolean }): string {
+export function encode(buffer: Uint8Array, options?: EncodeOptions): string {
   const alphabet = getAlphabet(options?.alphabet);
   const output = convert({ input: buffer, inputBase: 8, outputBase: 5, convertRemainingBits: true });
   if (options?.padding ?? true) {
@@ -63,10 +90,10 @@ export function encode(buffer: Uint8Array, options?: { alphabet?: string; paddin
  * Decodes a base32 encoded string into a Uint8Array buffer.
  *
  * @param string The base32 encoded string.
- * @param options Optionally specify the alphabet to use. The alphabet must be a 32-character string, or 33 characters if specifying the padding character to use
+ * @param options The options to use for decoding.
  * @returns The decoded buffer.
  */
-export function decode(string: string, options?: { alphabet?: string }): Uint8Array {
+export function decode(string: string, options?: DecodeOptions): Uint8Array {
   const alphabet = getAlphabet(options?.alphabet);
   const map = new Map(Array.from(alphabet).map((character, index) => [character, index]));
   const input = Array.from(string, (character) => require(map.get(character), `Invalid character ${character}`));
